85ce3f44549b2162c075c2362846ee90ba2997e6
Jyotimoy Kashyap <jyotimoykashyap123@gmail.com>
2020-12-18 15:34:17 +0530

    Change Log -
    1. Colors night xml file made for dark themes
    2. Lottie dependency added
    3. No data raw resource added for main recycler view
    4. Splash screen is made functional
    5. Action bar is hidden in splash screen
    
    Note:  Handler() is deprecated and we now we Looper for the delay method.

--- app/src/main/java/com/example/todoapp/fragments/SplashScreenFragment/SplashScreenFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/SplashScreenFragment/SplashScreenFragment.kt
@@ -1,14 +1,20 @@
 package com.example.todoapp.fragments.SplashScreenFragment
 
+import android.app.Activity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import com.example.todoapp.R
 
 
 class SplashScreenFragment : Fragment() {
+
 
 
     override fun onCreateView(
@@ -16,7 +22,14 @@ class SplashScreenFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+        (activity as AppCompatActivity).supportActionBar?.hide()
+        val view =  inflater.inflate(R.layout.fragment_splash_screen, container, false)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            view.findNavController().navigate(R.id.action_splashScreenFragment_to_todoListFragment)
+        },500)
+
+        return view
     }
 
 
--- app/src/main/java/com/example/todoapp/fragments/TodoListFragment/TodoListFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/TodoListFragment/TodoListFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import com.example.todoapp.R
 
 
@@ -14,7 +15,11 @@ class TodoListFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_todo_list, container, false)
+        (activity as AppCompatActivity).supportActionBar?.show()
+        val view = inflater.inflate(R.layout.fragment_todo_list, container, false)
+
+
+        return view
     }
 
 
