09a7d17c7010c1027d8483ae701d96894cd9e7cb
Jyotimoy Kashyap <jyotimoykashyap123@gmail.com>
2020-12-21 17:21:42 +0530

    Change Log -
    1. Added custom toolbar.
    2. Changes in themes.xml for appbar appearance.
    3. layout for list row done.

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(actionbar)
+
+        val appBarConfiguration = AppBarConfiguration
+            .Builder(R.id.todoListFragment)
+            .build()
+
+        setupActionBarWithNavController(findNavController(R.id.navhostfragment), appBarConfiguration)
+        supportActionBar?.elevation = 0f
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.navhostfragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/example/todoapp/fragments/TodoListFragment/TodoListFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/TodoListFragment/TodoListFragment.kt
@@ -6,7 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
 import com.example.todoapp.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.android.synthetic.main.fragment_todo_list.view.*
 
 
 class TodoListFragment : Fragment() {
@@ -18,7 +21,9 @@ class TodoListFragment : Fragment() {
         (activity as AppCompatActivity).supportActionBar?.show()
         val view = inflater.inflate(R.layout.fragment_todo_list, container, false)
 
-
+        view.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_todoListFragment_to_addTodoFragment)
+        }
         return view
     }
 
