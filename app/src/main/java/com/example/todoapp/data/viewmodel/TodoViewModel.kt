6db8320437f86628acc1ed9fb2dd2e4102993238
Jyotimoy Kashyap <jyotimoykashyap123@gmail.com>
2021-01-04 15:35:36 +0530

    Change Log -
    Viewmodel created fot the todo

---
+++ app/src/main/java/com/example/todoapp/data/viewmodel/TodoViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.todoapp.data.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.todoapp.data.TodoDatabase
+import com.example.todoapp.data.models.TodoData
+import com.example.todoapp.data.repository.TodoRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class TodoViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val todoDao = TodoDatabase.getDatabase(application).todoDao()
+    private val repository: TodoRepository
+
+
+    private val getAllData : LiveData<List<TodoData>>
+
+    init {
+        repository = TodoRepository(todoDao)
+        getAllData = repository.getAllData
+    }
+
+    fun insertData(todoData: TodoData){
+        viewModelScope.launch(Dispatchers.IO){
+            repository.insertData(todoData)
+        }
+    }
+}
