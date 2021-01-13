10a6a3ea16711484ac8fd7c8102a124028262de1
Jyotimoy Kashyap <jyotimoykashyap123@gmail.com>
2021-01-13 18:01:34 +0530

    Binding is set for TodoListAdapter.kt

--- app/src/main/java/com/example/todoapp/BindingAdapters.kt
+++ app/src/main/java/com/example/todoapp/BindingAdapters.kt
@@ -1,11 +1,15 @@
 package com.example.todoapp
 
+import android.renderscript.RenderScript
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import androidx.lifecycle.MutableLiveData
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.FragmentNavigatorExtras
+import com.example.todoapp.data.models.Priority
 import com.example.todoapp.fragments.TodoListFragment.TodoListFragmentDirections
+import com.google.android.material.card.MaterialCardView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class BindingAdapters {
@@ -30,6 +34,17 @@ class BindingAdapters {
             when(emptyDatabase.value){
                 true -> view.visibility = View.VISIBLE
                 false -> view.visibility = View.INVISIBLE
+            }
+        }
+
+
+        @BindingAdapter("android:parsePriorityColor")
+        @JvmStatic
+        fun parsePriorityColor(cardView: MaterialCardView, priority: Priority){
+            when(priority){
+                Priority.HIGH -> cardView.setCardBackgroundColor(ContextCompat.getColor(cardView.context, R.color.red))
+                Priority.MEDIUM -> cardView.setCardBackgroundColor(ContextCompat.getColor(cardView.context, R.color.yellow))
+                Priority.LOW -> cardView.setCardBackgroundColor(ContextCompat.getColor(cardView.context, R.color.green))
             }
         }
     }
--- app/src/main/java/com/example/todoapp/fragments/TodoListFragment/TodoListAdapter.kt
+++ app/src/main/java/com/example/todoapp/fragments/TodoListFragment/TodoListAdapter.kt
@@ -17,58 +17,71 @@ import java.util.zip.Inflater
 
 class TodoListAdapter(private val listener : TodoAdapterListener) : RecyclerView.Adapter<TodoListAdapter.MyViewHolder>() {
 
-    var dataList = emptyList<TodoData>()
+    private var dataList = emptyList<TodoData>()
 
     class MyViewHolder(val viewBinding: RowLayoutBinding) :
         RecyclerView.ViewHolder(viewBinding.root) {
+            fun bind(todoData: TodoData){
+                viewBinding.todoData = todoData
+                viewBinding.executePendingBindings()
+            }
+        companion object{
+            fun from(parent: ViewGroup) : MyViewHolder{
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = RowLayoutBinding.inflate(layoutInflater, parent, false)
+                return MyViewHolder(binding)
+            }
+        }
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val binding = RowLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return MyViewHolder(binding)
+        return MyViewHolder.from(parent)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.viewBinding.run {
-            titleTextView.text = dataList[position].title
-            descTextView.text = dataList[position].desc
+        val currentItem = dataList[position]
+        holder.bind(currentItem)
 
-            val priority = dataList[position].priority
-            when (priority) {
-                Priority.HIGH -> {priorityIndicator.setCardBackgroundColor(
-                    ContextCompat.getColor(
-                        holder.viewBinding.root.context,
-                        R.color.red
-                    )
-                )
-                    }
-                Priority.MEDIUM -> {priorityIndicator.setCardBackgroundColor(
-                    ContextCompat.getColor(
-                        holder.viewBinding.root.context,
-                        R.color.yellow
-                    )
-                )
-                    }
-                Priority.LOW -> {priorityIndicator.setCardBackgroundColor(
-                    ContextCompat.getColor(
-                        holder.viewBinding.root.context,
-                        R.color.green
-                    )
-                )
-                    }
-            }
+//        holder.viewBinding.run {
+//            titleTextView.text = dataList[position].title
+//            descTextView.text = dataList[position].desc
+//
+//            val priority = dataList[position].priority
+//            when (priority) {
+//                Priority.HIGH -> {priorityIndicator.setCardBackgroundColor(
+//                    ContextCompat.getColor(
+//                        holder.viewBinding.root.context,
+//                        R.color.red
+//                    )
+//                )
+//                    }
+//                Priority.MEDIUM -> {priorityIndicator.setCardBackgroundColor(
+//                    ContextCompat.getColor(
+//                        holder.viewBinding.root.context,
+//                        R.color.yellow
+//                    )
+//                )
+//                    }
+//                Priority.LOW -> {priorityIndicator.setCardBackgroundColor(
+//                    ContextCompat.getColor(
+//                        holder.viewBinding.root.context,
+//                        R.color.green
+//                    )
+//                )
+//                    }
+//            }
             //extras for the material container transform
-            val extras = FragmentNavigatorExtras(todoCardItem to "container_transform_for_row_item")
-            todoCardItem.transitionName = (R.string.todo_item_card + dataList[position].id).toString()
+            val extras = FragmentNavigatorExtras(holder.viewBinding.todoCardItem to "container_transform_for_row_item")
+            holder.viewBinding.todoCardItem.transitionName = (R.string.todo_item_card + dataList[position].id).toString()
 
             //on item clicked
-            todoCardItem.setOnClickListener{
-                listener.onTodoClicked(todoCardItem, dataList[position])
+            holder.viewBinding.todoCardItem.setOnClickListener{
+                listener.onTodoClicked(holder.viewBinding.todoCardItem, dataList[position])
             }
-
-
-        }
+//
+//
+//        }
 
 
     }
